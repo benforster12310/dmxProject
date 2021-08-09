0ab6a7114e4907f82205585c97ec772f5be210cf
benforster12310 <forster.ben66@gmail.com>
2021-08-09 21:46:22 +0100

    Save Change V0.0.8

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,27 @@
-const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+var ipc = require("electron").ipcMain;
+
+const SerialPort = require("serialport");
+
+// list the vendorid for arduino and then creat a list of the productids with the descriptions on them
+let arduinoVendorId = 2341
+let arduinoProductIds = {
+    "0001":  "Uno",
+	"0036":  "Leonardo Bootloader",
+	"0010":  "Mega 2560",
+	"003b":  "Serial Adapter",
+	"003d":  "Due Programming Port",
+	"003e":  "Due",
+	"003f":  "Mega ADK",
+	"0042":  "Mega 2560 R3",
+	"0043":  "Uno R3",
+	"0044":  "Mega ADK R3",
+	"0045":  "Serial R3",
+	"0049":  "ISP",
+	"8036":  "Leonardo",
+	"8038":  "Robot Control Board"
+}
+
 
 function createWindow(width, height, file, maximised) {
     const win = new BrowserWindow({
@@ -35,5 +58,18 @@ app.on('window-all-closed', function () {
 
 // then handle IPC events
 
-
+ipc.on("SerialPortsList", function(event, data) {
+    let portsObject = {};
+    SerialPort.list().then(function(data) {
+        // then go through each device
+        for(var i = 0; i < data.length; i++) {
+            // then check if the device is made by arduino
+            if(data[i].vendorId == arduinoVendorId) {
+                // then add it to the portsList with the description
+                portsObject[Object.keys(portsObject).length] = {"port": data[i].path, "name": arduinoProductIds[data[i].productId]}
+            }
+        }
+        event.sender.send("SerialPortsListResponse", JSON.stringify(portsObject))
+    });
+})
 
---
+++ electron/pages/index.js
@@ -0,0 +1,48 @@
+var ipc = require("electron").ipcRenderer;
+const dialog = require("electron").remote.dialog;
+
+// then handle alerts and confirms
+const browserWindow = require('electron').remote.getCurrentWindow()
+
+alert = function(txt) {
+    dialog.showMessageBoxSync(browserWindow, {"message": txt, "type":"question"})
+}
+confirm = function(txt) {
+    let retVal = dialog.showMessageBoxSync(browserWindow, {"message": txt, "type":"error", "buttons": ["OK", "Cancel"], "defaultId":0, "cancelId":1})
+    if(retVal == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// then ask the main process for the SerialPorts list
+ipc.on("SerialPortsListResponse", function(event, data) {
+    // then parse the json
+    let responseObject = JSON.parse(data)
+    document.getElementById("dmxInterfaceSelect").innerHTML = "";
+    // then go through each key and then append an option element to the select list
+    for(var i = 0; i < Object.keys(responseObject).length; i++) {
+        let optionElement = document.createElement("option");
+        let optionElementTextNode = document.createTextNode("Arduino " + responseObject[i].name + " @" + responseObject[i].port);
+        optionElement.appendChild(optionElementTextNode)
+        optionElement.setAttribute("value", responseObject[i].port)
+        document.getElementById("dmxInterfaceSelect").appendChild(optionElement);
+    }
+    if(Object.keys(responseObject).length == 0) {
+        let optionElement = document.createElement("option");
+        let optionElementTextNode = document.createTextNode("No Devices Are Connected");
+        optionElement.appendChild(optionElementTextNode)
+        optionElement.setAttribute("value", null)
+        document.getElementById("dmxInterfaceSelect").appendChild(optionElement);
+    }
+})
+
+let refreshListBtn = document.getElementById("refreshListBtn");
+refreshListBtn.addEventListener("click", getSerialDevices);
+
+function getSerialDevices() {
+    ipc.send("SerialPortsList", "");
+}
+getSerialDevices()
