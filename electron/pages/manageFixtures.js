50aed2c3c28783655aecf2c8e6a02235530a6f23
Ben Forster <forster.ben65@gmail.com>
2021-08-23 12:25:23 +0100

    SAVE CHANGE v0.0.17

--- electron/pages/manageFixtures.js
+++ electron/pages/manageFixtures.js
@@ -17,6 +17,17 @@ confirm = function(txt) {
     }
 }
 
+// then define the Color class
+class Color {
+    constructor(offValue, minValue, maxValue, colorChannel, dimmableViaOnChannel) {
+        this.off = offValue;
+        this.min = minValue;
+        this.max = maxValue;
+        this.channel = colorChannel;
+        this.dimmableViaOnChannel = dimmableViaOnChannel;
+    }
+}
+
 // then fetch all of the fixtures
 ipc.on("SettingsGetFixturesResponse", function(event, data) {
     let dataObject = JSON.parse(data);
@@ -34,6 +45,8 @@ ipc.send("SettingsGetFixtures", "");
 
 var fixturesObject = null;
 var fixturesObjectLength
+
+let isEditingFixture = false;
 
 function listFixtures(dataObject) {
     // then look inside the dataObject for the fixtures object and count them
@@ -61,6 +74,7 @@ let colorsObject = {};
 
 // edit fixture function
 function editFixture(fixtureId) {
+    isEditingFixture = true;
     let fixture = fixturesObject[fixtureId];
     // then fill the fields in
     document.getElementById("editFixtureDiv_fixtureName").value = fixture.name;
@@ -70,16 +84,56 @@ function editFixture(fixtureId) {
     document.getElementById("editFixtureDiv_onChannelValue").value = fixture.onChannelValue;
     colorsObject = fixture.colors;
     editFixtureDiv_updateColors();
+    document.getElementById("fixturesDiv").classList.add("hidden");
+    document.getElementById("editFixtureDiv").classList.remove("hidden");
 }
 
 function editFixtureDiv_addColor() {
-    
+    // then hide the editFixtureDiv and open the addColorDiv
+    alert("hi")
+    document.getElementById("editFixtureDiv").classList.add("hidden");
+    document.getElementById("addColorDiv").classList.remove("hidden");
 }
 
-function editFixtureDiv_removeColor() {
-
+function editFixtureDiv_removeColor(cid) {
+    if(confirm("Are You Sure That You Want To Remove The Color '" + Object.keys(colorsObject)[cid] + "' From The Fixture") == true) {
+        // then delete the color
+        delete colorsObject[Object.keys(colorsObject)[cid]];
+        console.log(colorsObject);
+        // then update the color
+        editFixtureDiv_updateColors();
+    }
 }
 
 function editFixtureDiv_updateColors() {
+    // then display the colors as a button
+    document.getElementById("editFixtureDiv_colorsDiv").innerHTML = '<button class="btn button fullWidth" onclick="editFixtureDiv_addColor()">Add Color</button><br/><br/>';
+    for(var i = 0; i < Object.keys(colorsObject).length; i++) {
+        let button = document.createElement("button");
+        button.setAttribute("class", "btn button fullWidth");
+        let buttonTextNode = document.createTextNode(Object.keys(colorsObject)[i]);
+        button.appendChild(buttonTextNode);
+        button.setAttribute("onclick", "editFixtureDiv_removeColor(" + i + ")");
+        document.getElementById("editFixtureDiv_colorsDiv").appendChild(button);
+    }
+}
 
+// addColorDiv
+
+function addColorDiv_addColor() {
+    // then get all of the data and collate it into a new Color
+    let colorName = document.getElementById("addColorDiv_colorName").value;
+    let colorObject = new Color(parseInt(document.getElementById("addColorDiv_colorOffValue").value), parseInt(document.getElementById("addColorDiv_colorMinValue").value), parseInt(document.getElementById("addColorDiv_colorMaxValue").value), parseInt(document.getElementById("addColorDiv_colorDmxChannel").value), document.getElementById("addColorDiv_dimmableViaOnChannel").checked);
+    // then if isEditingFixture is set to false
+    if(isEditingFixture == false) {
+
+    }
+    else {
+        // then add the color to the colorsObject
+        console.log(colorObject);
+        colorsObject[colorName] = colorObject;
+        editFixtureDiv_updateColors();
+        document.getElementById("addColorDiv").classList.add("hidden");
+        document.getElementById("editFixtureDiv").classList.remove("hidden");
+    }
 }
