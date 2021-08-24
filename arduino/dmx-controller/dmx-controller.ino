d92e5da984db764cef524750c4926e19c2394b8e
benforster12310 <forster.ben66@gmail.com>
2021-08-24 19:02:41 +0100

    SAVE CHANGE v0.0.22

--- arduino/dmx-controller/dmx-controller.ino
+++ arduino/dmx-controller/dmx-controller.ino
@@ -1,18 +1,40 @@
+#include <ArduinoJson.h>
+#include <DmxSimple.h>
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("ready");
+  DmxSimple.usePin(3);
+  DmxSimple.maxChannel(70);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   while(Serial.available() > 0) {
-    String str = Serial.readString();
-    if(str == "alive\n") {
-      Serial.println("true");
+    String serialLine = Serial.readString();
+    if(!serialLine.startsWith("{")) {
+      // then check if its a config message
+      if(serialLine == "alive\n") {
+        Serial.println("true");
+      }
     }
     else {
-      // then it must be a value to set the dmx to so handle that here
+      // then define the jsonDocument
+      StaticJsonDocument<48> doc;
+      DeserializationError error = deserializeJson(doc, serialLine);
+      if(error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+      }
+
+      // then write the channel
+      DmxSimple.write(doc["channel"], doc["value"]);
+      Serial.print("WRITTEN ");
+      Serial.print(doc["value"]);
+      Serial.print(" TO ");
+      Serial.println(doc["channel"]);
     }
   }
 }
